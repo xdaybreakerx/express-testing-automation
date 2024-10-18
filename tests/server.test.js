bd3e3d4d212670dacf85b3f41ddf3040d47e63b7
Xander Salathe <xsalathe@gmail.com>
2024-10-19 10:27:20 +1100

    feat: test root route

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,15 +1,20 @@
-const { app } = require("../src/server.js");
-const request = require("supertest");
+const { app } = require('../src/server.js');
+const request = require('supertest');
 
-describe("Root route", () => {
+describe('Root route', () => {
     test("server returns a 'hello world!' message", async () => {
-        // make a request to "/" route 
+        // make a request to "/" route
         // wait for response and store it
-        const response = await request(app).get("/");
+        const response = await request(app).get('/');
 
-        // check the body of the response for a "Hello world!" string 
-        expect(response.body.message).toBe("hello world!");
-
+        // check the body of the response for a "Hello world!" string
+        expect(response.body.message).toBe('hello world!');
+        // expect(response.t).toBe(200);
     });
 
+    test("Server returns a response with no auth headers because we are not sending auth data", async () => {
+        const response = await request(app).get("/");
+
+        expect(response.headers["Authorization"]).toBeFalsy();
+    })
 });

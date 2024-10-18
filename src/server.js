a7674fd28af5e2f9d22c3a045d530cc377d3b1f4
Xander Salathe <xsalathe@gmail.com>
2024-10-19 10:03:08 +1100

    feat: project init

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// make an instance of the express server 
+const app = express();
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "hello world!"
+    });
+});
+
+module.exports = {
+    app
+}
